8b7e1b87f383d1413a92ef7df2f2a03d4f17773e
Artur <artur@localhost.localdomain>
2019-08-21 16:55:26 -0300

    usando os operadores aritmeticos

---
+++ aritmeticos /calculos.py
@@ -0,0 +1,45 @@
+# soma 
+print ("soma")
+print (10+10)
+print ("\n")
+# subtracao 
+print ("subtracao")
+print (10-5)
+print ("\n")
+#multiplicacao 
+print ("multiplicacao")
+print (10*2)
+print ("\n")
+#divisao
+print ("divisao")
+print (10/5)
+print ("\n")
+#quadrado 
+print ("quadrado")
+print (10**2)
+print ("\n")
+
+#Cuidado com os parenteses, eles fazem muita diferenca 
+
+print ("exemplo 1")
+print (10+5*(10))
+
+print ("exemplo 2") 
+print ((10+5)*(10))
+
+print ("exemplo 3")
+print ((10*5)+(10*2)-5)
+print ("\n")
+#numeros float 
+print ("float")
+#soma
+print (0.1 + 0.2)
+print ("\n")
+#multiplicacao
+print (0.1*0.2)
+print ("\n")
+#subtracao 
+print (0.5-0.4)
+print ("\n")
+#divisao 
+print (0.5/0.5)
---
+++ aritmeticos /calculos.rb
@@ -0,0 +1,45 @@
+# soma 
+puts ("soma")
+puts (10+10)
+puts ("\n")
+# subtracao 
+puts ("subtracao")
+puts (10-5)
+puts ("\n")
+#multiplicacao 
+puts ("multiplicacao")
+puts (10*2)
+puts ("\n")
+#divisao
+puts ("divisao")
+puts (10/5)
+puts ("\n")
+#quadrado 
+puts ("quadrado")
+puts (10**2)
+puts ("\n")
+
+#Cuidado com os parenteses, eles fazem muita diferenca 
+
+puts ("exemplo 1")
+puts (10+5*(10))
+
+puts ("exemplo 2") 
+puts ((10+5)*(10))
+
+puts ("exemplo 3")
+puts ((10*5)+(10*2)-5)
+puts ("\n")
+#numeros float 
+puts ("float")
+#soma
+puts (0.1 + 0.2)
+puts ("\n")
+#multiplicacao
+puts (0.1*0.2)
+puts ("\n")
+#subtracao 
+puts (0.5-0.4)
+puts ("\n")
+#divisao 
+puts (0.5/0.5)
